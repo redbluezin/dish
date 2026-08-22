2ceac3e9d0fab34f69cc51b3cc7038e0506bac6d
zinonSk <gostos4put4@gmail.com>
2026-08-22 11:45:18 -0300

    Refactor dish.py to Python script with improved handling
    Refactor dish.py to use Python instead of shell commands for dependency checks and execution. Improved error handling and output display.

--- termux_DS/dish.py
+++ termux_DS/dish.py
@@ -1,40 +1,33 @@
-#!/data/data/com.termux/files/usr/bin/sh
+import subprocess as sp
+import os, sys
 
-# =========================================================
-# Dependências
-# =========================================================
+if len(sys.argv) >= 2 :
+    with open(f"{os.getcwd()}/{sys.argv[1]}","r") as rl :
+        mx = rl.read()
+else :
+    raise ValueError("dish: erro, formatação Invalida")
 
-if ! command -v python >/dev/null 2>&1; then
-    echo "Python não encontrado."
-    pkg install python -y
-fi
+mx = mx.split("\n")
+hh = sys.argv[1:]
 
-if ! command -v git >/dev/null 2>&1; then
-    echo "Git não encontrado."
-    pkg install git -y
-fi
+for vn, ps in enumerate(mx) :
+    if ps.startswith("import") :
+        vf = ps.split()[1]
+        if "/" in vf:
+            with open(vf, "r") as r :
+                vm = r.read()
+        else :
+            with open(f"/storage/emulated/0/testes/tu2/{vf}", "r") as r :
+                vm = r.read()
+        mx[vn] = f"{vm}\n"
+    else :
+        mx[vn] = ps+"\n"
 
+p = "".join(mx)
+mb = sp.run(["sh","-c", p, *hh], capture_output=True, text=True)
 
-# =========================================================
-# Instalação / atualização
-# =========================================================
-
-if [ -d "$HOME/.dish/.git" ]; then
-
-    echo "Atualizando dish..."
-    git -C "$HOME/.dish" pull
-
-elif [ -d "$HOME/dish/termux_DS" ]; then
-
-    echo "Migrando dish para .dish..."
-    mv "$HOME/dish" "$HOME/.dish"
-
-else
-
-    echo "Baixando dish..."
-    git clone https://github.com/redbluezin/dish "$HOME/.dish"
-
-fi
+print(mb.stdout)
+print(mb.stderr)fi
 
 
 # =========================================================
